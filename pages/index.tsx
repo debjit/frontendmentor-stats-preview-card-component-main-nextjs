4902c5f6734167f0c4fe9867f6b2e7d05feae757
Debjit Biswas <debjit012@gmail.com>
2022-11-23 14:08:19 +0530

    Update new code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,51 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <main>
+      <div className="flex items-center justify-center h-screen bg-blue-900">
+        <div className="max-w-md bg-blue-700 text-white rounded-md shadow-md overflow-hidden md:max-w-4xl my-10 mx-3 md:my-0 md:mx-0">
+          <div className="flex flex-col-reverse md:flex md:flex-row">
+            <div className="p-10 text-center md:text-left">
+              <h2 className="text-4xl text-bold">
+                Get <span className="text-purple-900">insights</span> that help
+                your business grow.
+              </h2>
+              <p className="py-6">
+                Discover the benefits of data analytics and make better
+                decisions regarding revenue, customer experience, and overall
+                efficiency.
+              </p>
+              <div className="uppercase md:grid md:grid-cols-3 ">
+                <div className=" my-4 ">
+                  <p className="text-2xl font-bold">10k+</p>
+                  <p>companies</p>
+                </div>
+                <div className=" my-4 ">
+                  {" "}
+                  <p className="text-2xl font-bold">314</p>
+                  <p>templates</p>
+                </div>
+                <div className=" my-4 ">
+                  {" "}
+                  <p className="text-2xl font-bold">12m+</p> <p>Queries</p>
+                </div>
+              </div>
+            </div>
+            <div className="md:shrink-0 md:w-1/2">
+              <img
+                className="h-48 w-full object-cover inset-x-0 md:h-full md:w-full"
+                src="/images/image-header-mobile.jpg"
+                alt="Modern building architecture"
+              />
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
+    </main>
+  );
+};
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
